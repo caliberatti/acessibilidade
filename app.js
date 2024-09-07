cffe69d793fc5c95689eb07eb8f1292ca2ade295
Carolina Liberatti <141589092+caliberatti@users.noreply.github.com>
2024-09-07 10:49:51 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa");
+
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+
+    campoPesquisa = campoPesquisa.toLowerCase();
+
+
+    let resultados = "";
+    let titulo = "";
+    let endereco = "";
+    let tags = "";
+
+
+    //para cada dado dentro da lista de dados
+    for (let dado of dados) {
+        titulo = dado?.titulo?.toLowerCase();
+        endereco = dado?.endereco?.toLowerCase();
+        link = dado?.linkentradaacessivel?.toLowerCase();
+        tags = dado?.tags?.toLowerCase();
+
+
+        if (titulo.includes(campoPesquisa) || endereco.includes(campoPesquisa) || link.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            resultados += `
+            <div class="item-resultado">
+                <h2 class="titulo">${dado?.titulo}</h2>
+                <h5 class="endereco">${dado?.endereco}</h5>
+                <a href="${dado?.linkentradaacessivel}" target="_blank">entrada acessivel para cadeira de rodas</a> <br>
+                <a href="${dado?.linkespaco}" target="_blank">espaço entre as mesas</a> <br>
+                <a href="${dado?.estacionamento}" target="_blank">estacionamento</a> <br>
+                <a href="${dado?.linkguia}" target="_blank">guia rebaixada</a> <br>
+                ${dado.linkescada ? `<a href="${dado.linkescada}" target="_blank">acesso ao 2º andar apenas via escada</a> <br>` : ''}
+    
+            </div>
+        `;
+        }
+
+
+    }
+
+
+    if (!resultados) {
+        resultados = `<p>Nada foi encontrado</p>`
+    }
+    console.log(resultados)
+    section.innerHTML = resultados;
+
+
+   
+
+
+}
+// if (campoPesquisa === "") {
+//         pesquisar()    
+//     }
+
+
+
+
+window.onload=pesquisar;
---
+++ dados.js
@@ -0,0 +1,61 @@
+let dados = [
+    {
+        titulo:"Padaria Casaria - Padaria",
+        endereco: "Avenida Conselheiro Nébias, 718 - Boqueirão, Santos.",
+        linkentradaacessivel: "https://maps.app.goo.gl/HuB3K8ewHHvjdq2E9",
+        linkespaco: "https://maps.app.goo.gl/zQgPSUGhMEnhkguq9",
+        linkestacionamento: "https://maps.app.goo.gl/Sf4ccY3pb3WbwFQa9",
+        linkguia: "https://www.google.com/maps/place/Casaria+Padaria/@-23.9658211,-46.3236899,3a,75y,90t/data=!3m7!1e1!3m5!1sTrWVwGD6cS4TUfJV5iUVBw!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D0%26panoid%3DTrWVwGD6cS4TUfJV5iUVBw%26yaw%3D0!7i16384!8i8192!4m9!3m8!1s0x94ce03b0bdc8fa5f:0x59438b6ba02c0089!8m2!3d-23.9658882!4d-46.323964!10e5!14m1!1BCgIgARICCAI!16s%2Fg%2F11nwz8wstw?authuser=0&coh=205410&entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D",
+        tags: "boqueirao Boqueirao padoca"
+        
+    },
+    {
+        titulo:"Osteria Santos Sabores - Restaurante Italiano",
+        endereco: "Rua Dom Lara, 65 - Boqueirão, Santos.",
+        linkentradaacessivel: "https://maps.app.goo.gl/czWSwiTe4M2WEpuk7",
+        linkespaco: "https://maps.app.goo.gl/66uwWFxqfB7fFHAa7",
+        linkestacionamento: "https://maps.app.goo.gl/32nNs9YtK48WBpqdA",
+        linkguia: "https://www.google.com/maps/place/Osteria+Santos+Sabores/@-23.9681087,-46.3215224,3a,75y,166.91h,90t/data=!3m7!1e1!3m5!1sQe5M_YcHovpyoobf1CLikw!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DQe5M_YcHovpyoobf1CLikw%26cb_client%3Dmaps_sv.tactile.gps%26w%3D203%26h%3D100%26yaw%3D166.9087%26pitch%3D0%26thumbfov%3D100!7i16384!8i8192!4m9!3m8!1s0x94ce025381369c01:0xa5e78c8a49c4fc00!8m2!3d-23.9682635!4d-46.321484!10e5!14m1!1BCgIgARICCAI!16s%2Fg%2F11b6_th8hw?authuser=0&coh=205409&entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D",
+        tags: "boqueirao Boqueirao"
+
+    },
+    {
+        titulo:"Nagoya One - Restaurante Japonês",
+        endereco: "Avenida Siqueira Campos, 564 - Boqueirão, Santos.",
+        linkentradaacessivel: "https://maps.app.goo.gl/edLQNsDdD7GyA9Zt5",
+        linkespaco: "https://maps.app.goo.gl/AfwV9zd3vV9E1JqK9",
+        linkestacionamento: "https://maps.app.goo.gl/R2hDirmLo9DtTXQK8",
+        linkguia: "https://www.google.com/maps/place/Restaurante+Nagoya+One+-+Sushi/@-23.9684289,-46.3199515,3a,75y,243.14h,73.66t/data=!3m8!1e1!3m6!1sAF1QipMQpVtwPX7QhDGdiQ091AaMj4HWWXMpXPGV-s0c!2e10!3e11!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipMQpVtwPX7QhDGdiQ091AaMj4HWWXMpXPGV-s0c%3Dw203-h100-k-no-pi-22.283909-ya298.7826-ro-3.4139566-fo100!7i7200!8i3600!4m9!3m8!1s0x159f1f6470c126f:0x19b542d2295ef468!8m2!3d-23.9685747!4d-46.3200714!10e5!14m1!1BCgIgARICCAI!16s%2Fg%2F1q63ckszp?authuser=0&coh=205409&entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D",
+        tags: "japones boqueirao Boqueirao"
+    },
+    {
+        titulo:"Kyuurai - Restaurante Japonês",
+        endereco: "Avenida Siqueira Campos, 555 - Embaré, Santos.",
+        linkentradaacessivel: "https://maps.app.goo.gl/tMgp4rZgDfBvS2Wz9",
+        linkespaco: "https://maps.app.goo.gl/JYTxSsTAeLwqaDuH9",
+        linkestacionamento: "https://www.google.com/maps/place/Kyuurai/@-23.9679828,-46.3196385,3a,75y,95.55h,90t/data=!3m7!1e1!3m5!1s-7sUPOE40oVeifeH0L5uHQ!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D0%26panoid%3D-7sUPOE40oVeifeH0L5uHQ%26yaw%3D95.55309!7i16384!8i8192!4m9!3m8!1s0x94ce03ab8136e987:0x4ae87860f0d7a9ce!8m2!3d-23.9679878!4d-46.3195797!10e5!14m1!1BCgIgARICCAI!16s%2Fg%2F1pty5gxmn?authuser=0&coh=205410&entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D",
+        linkguia: "https://www.google.com/maps/place/Kyuurai/@-23.9679828,-46.3196385,3a,75y,95.55h,90t/data=!3m7!1e1!3m5!1s-7sUPOE40oVeifeH0L5uHQ!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D0%26panoid%3D-7sUPOE40oVeifeH0L5uHQ%26yaw%3D95.55309!7i16384!8i8192!4m9!3m8!1s0x94ce03ab8136e987:0x4ae87860f0d7a9ce!8m2!3d-23.9679878!4d-46.3195797!10e5!14m1!1BCgIgARICCAI!16s%2Fg%2F1pty5gxmn?authuser=0&coh=205410&entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D",
+        linkescada: "https://maps.app.goo.gl/L1rmoh6mAh9v92Tq5",
+        tags: "japones boqueirao Boqueirao"
+    },
+    {
+        titulo:"Katsuya Sushi - Restaurante Japonês",
+        endereco: "Rua Professor Primo Ferreira, 6 - Boqueirão, Santos.",
+        linkentradaacessivel: "https://maps.app.goo.gl/SbZxM7efR77noPGTA",
+        linkespaco: "https://maps.app.goo.gl/xRzAEurgcbimoFL66",
+        linkestacionamento: "https://maps.app.goo.gl/jyGJ2hLxuy8Xc5fQA",
+        linkguia: "https://maps.app.goo.gl/HJkMh91LKBcFJ6TE7",
+        tags: "japones boqueirao Boqueirao"
+    },
+    {
+        titulo:"Creperia & Oásis da Praia - Creperia",
+        endereco: "Avenida San Martin, 138 - Ponta da Praia, Santos.",
+        linkentradaacessivel: "https://maps.app.goo.gl/zBDZqiBS64Yhf1gf7",
+        linkespaco: "https://maps.app.goo.gl/NZc67EetQgjNeZ597",
+        linkestacionamento: "https://www.google.com/maps/place/Creperia+%26+O%C3%A1sis+da+Praia+-+C7/@-23.9851955,-46.2998694,3a,75y,276.08h,90t/data=!3m7!1e1!3m5!1sEhJgkbiw22ISWyVJKDSf1w!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DEhJgkbiw22ISWyVJKDSf1w%26cb_client%3Dmaps_sv.tactile.gps%26w%3D203%26h%3D100%26yaw%3D276.07962%26pitch%3D0%26thumbfov%3D100!7i16384!8i8192!4m9!3m8!1s0x94ce02169b2afa65:0x641009242d69335a!8m2!3d-23.9851802!4d-46.3000385!10e5!14m1!1BCgIgARICCAI!16s%2Fg%2F11c5x84z68?authuser=0&coh=205409&entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D",
+        linkguia: "https://maps.app.goo.gl/TYRCFHvmneCL6Zkd8",
+        linkescada: "https://maps.app.goo.gl/JoJ8acPNKatTuwtY7",
+        tags: "crepe canal 7"
+    }
+];
+
